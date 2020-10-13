4226bdc06e030ae2f88632ec4ce4e339a01c7ef2
Reynaldo Tiburcio <reynaldo.tiburcio@bee.io>
2020-10-13 12:46:34 +0200

    First commit

---
+++ src/build/precache-manifest.92c09712cfb1608d3ea64483dcbb32db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "725443f483cf364ea2aff5c54116e2c0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b18f9777c80a510736c3",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "8520178d2c49134ee522",
+    "url": "/static/js/2.bc37484a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.bc37484a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b18f9777c80a510736c3",
+    "url": "/static/js/main.f06fdbdb.chunk.js"
+  },
+  {
+    "revision": "8931283fa7532b66ee7f",
+    "url": "/static/js/runtime-main.fca178c7.js"
+  }
+]);
